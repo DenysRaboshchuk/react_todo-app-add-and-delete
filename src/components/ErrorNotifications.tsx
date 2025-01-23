cd25a8afd5297262744f2271ef94e5ed00bebd4a
Denys Raboshchuk <kontroler444@gmail.com>
2025-01-23 15:02:26 +0200

    Create React Todo App with Add and Delete features

---
+++ src/components/ErrorNotifications.tsx
@@ -0,0 +1,49 @@
+import classNames from 'classnames';
+import { useEffect } from 'react';
+
+type Props = {
+  errorMessage: string;
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const ErrorNotifications: React.FC<Props> = ({
+  errorMessage,
+  setErrorMessage,
+}) => {
+  useEffect(() => {
+    if (!errorMessage) {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setErrorMessage('');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [errorMessage, setErrorMessage]);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification',
+        'is-danger',
+        'is-light',
+        'has-text-weight-normal',
+        { hidden: !errorMessage },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => {
+          setErrorMessage('');
+        }}
+      />
+      {errorMessage}
+    </div>
+  );
+};
